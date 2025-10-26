186da232fe50988a7847a452d2bb4e7b0ae719f9
CrbyPatyy <macepilapil74.mp@gmail.com>
2025-10-26 21:42:14 +0800

    Update script.js

--- script.js
+++ script.js
@@ -120,7 +120,7 @@ const roomData = [
         features: ['Space', 'Comfort', 'Connection'],
         price: '2,100/night',
         images: ['https://images.unsplash.com/photo-1586023492125-27b2c045efd7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=800&q=80'],
-        link: 'Booking/booking.html'
+        link: 'room/doubleroom.html'
     },
     {
         id: 'barkada-room',
@@ -997,6 +997,7 @@ window.addEventListener("scroll", () => {
 
   scrollContainer.addEventListener("scroll", handleScroll, { passive: true });
 })();
+
 
 
 
